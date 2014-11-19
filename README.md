7ecacbd7f3626561f38723ab880fb54fe18d5b7d
Thomas Alexander <thomasalexander2718@gmail.com>
2014-11-19 11:08:48 -0500

    quick script to delete all favourites

---
+++ README.md
@@ -0,0 +1,32 @@
+Twitter Unfavourite All
+=======================
+
+Use this script to unfavourite all of your favourites. 
+
+
+### Caution - Use at your own risk. 
+
+## Installation
+
+You must have Python 2.7.x installed on your system and on the command line. 
+On Linux this is easy enough, but if you are on windows I recommend [Anaconda](https://store.continuum.io/cshop/anaconda/).
+
+Once Python is installed the only other requirement is the [Tweepy](https://github.com/tweepy/tweepy). Install with
+
+    pip install tweepy
+
+Finally download this repository by either clicking on the zip button on Github, or cloning the repository. 
+
+## Running
+
+Navigate to where you downloaded this repository and enter the project folder. Open "unfavorite.py" and enter
+the required information for your account. You will need to register an app on Twitter. No worries, it is quick 
+and painless. 
+
+Once everything is setup run 
+
+    python unfavourite.py 
+
+All done!! 
+
+-whitewhim
---
+++ unfavourite.py
@@ -0,0 +1,37 @@
+"""
+Use this to remove all of your followers on Twitter. It seems it is easy to gather 
+followers but not so easy to get rid of them, I wonder if Jesus ever felt this way. 
+
+Disclaimer: Use at your own risk. I am not responsible for anything that goes wrong.
+Don't be an idiot; the cardinal rule. 
+
+To run you must create an application at https://apps.twitter.com and get the consumer 
+and consumer secret keys. You must also create both access and access secret tokens on 
+the same page. Enter these keys below. 
+
+"""
+import tweepy
+
+consumer_key = "#"
+consumer_secret = "#"
+access_token = "#"
+access_token_secret = "#"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+batch_number = 100
+
+favs = api.favorites(count=batch_number)
+num_destroyed = 0
+while len(favs) > 0:
+	for f in favs:
+		api.destroy_favorite(f.id)
+		num_destroyed += 1
+		if num_destroyed % 100 == 1:
+			print("{} favourites have been destroyed".format(num_destroyed))
+	favs = api.favorites(count=batch_number)
+
+print("{} favourites destroyed in total".format(num_destroyed))
